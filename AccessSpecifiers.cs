8212a0021c3d7ede1a9b8e4528c09e830e4d5305
ArchanaABillava <archanaabillava0154@gmail.com>
2022-10-01 15:45:33 +0530

    Add project files.

---
+++ AccessSpecifiers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training_CSharp
+{
+    /// <summary>
+    /// Access Specifiers - Visibility
+    /// 5 types - public,private,protected(inheritance),internal,protected internal 
+    /// class - default access specifier - internal
+    /// variables - default access specifier - private
+    /// </summary>
+    public class AccessSpecifiers
+    {
+        public int eid;
+        public string ename;
+        //can access only within the class
+        private int age;
+        public void EmpDetails(int id, string name)
+        {
+            eid = id;
+            ename = name;
+            Console.WriteLine($"{ename}:{eid}");
+        }
+    }
+    public class AccessSpecifiersMain
+    {
+        public static void Main()
+        {
+            AccessSpecifiers accessSpecifiers = new AccessSpecifiers();
+            accessSpecifiers.EmpDetails(1, "John");
+            //accessSpecifiers.age = 25;//inaccessible outside the class
+            //Console.WriteLine(accessSpecifiers.age);//inaccessible outside the class
+
+        }
+    }
+}
