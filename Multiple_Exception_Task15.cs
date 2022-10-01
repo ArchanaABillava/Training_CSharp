84b036f43a137de5d84ecaec3ee1374a5fbfafde
ArchanaABillava <archanaabillava0154@gmail.com>
2022-10-01 17:15:21 +0530

    Commit_Task_15

---
+++ Multiple_Exception_Task15.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training_CSharp
+{
+    public class task15
+    {
+        //float result = 0;
+        /// <summary>
+        /// DivideByZeroException 
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+
+        /// <summary>
+        /// IndexOutOfRangeException
+        /// </summary>
+
+        /// <summary>
+        /// Multiple catch blocks
+        /// </summary>
+        public void MultipleCatch()
+        {
+            int[] a = { 10, 8, 6, 4, 8, 2 };
+            int[] b = { 5, 0, 3, 0, 2 };
+            for (int i = 0; i < a.Length; i++)
+            {
+                try
+                {
+                    Console.WriteLine(a[i] / b[i]);
+                }
+                catch (DivideByZeroException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+
+        }
+
+    }
+
+    internal class Multiple_Exception_Task15
+    {
+        public static void Main()
+        {
+            task15 ex = new task15();
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("----------Task-15(Multiple Catch-----------");
+            Console.WriteLine("-------------------------------------------");
+            ex.MultipleCatch();
+        }
+    }
+}
---
+++ UserValidate_Task15.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training_CSharp
+{
+    /// <summary>
+    /// Step 1 :Create the user-defined exception class
+    /// </summary>
+    public class UserValidate : ApplicationException
+    {
+        //Constructor 
+        public UserValidate(string message) : base(message)
+        {
+
+        }
+    }
+    /// <summary>
+    /// Step 2:Implementing the exception in the try and catch block
+    /// </summary>
+    internal class UserValidate_Task15
+    {
+        public static void Main()
+        {
+            int total_trainees;
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("-----Task-15 User Defined Validation-----");
+            Console.WriteLine("-----------------------------------------");
+            try
+            {
+                Console.WriteLine("Enter Your age");
+                total_trainees = Convert.ToInt32(Console.ReadLine());
+                if (total_trainees < 22)
+                {
+                    throw (new UserValidate("You are not eligible "));
+                }
+                else
+                {
+                    Console.WriteLine("Eligible");
+                }
+            }
+            catch (UserValidate e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
